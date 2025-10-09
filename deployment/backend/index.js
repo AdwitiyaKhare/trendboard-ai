0937d4165d7b5d9e1aa9aac2c1966c7e54e88687
AdwitiyaKhare <adwitiyakhare222004@gmail.com>
2025-10-09 12:29:28 +0530

    fix: CORS origin for deployed frontend

--- deployment/backend/index.js
+++ deployment/backend/index.js
@@ -10,7 +10,12 @@ const app = express();
 
 // ✅ Middlewares
 app.use(express.json());
-app.use(cors({ origin: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 const parser = new Parser();
 
